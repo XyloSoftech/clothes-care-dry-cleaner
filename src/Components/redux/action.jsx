85517a8bb1636f2cc5d1a23d69546d8e0fabe121
Taimoorali314 <132251064+Taimoorali314@users.noreply.github.com>
2024-03-26 02:02:28 +0500

    Shopping cart page updated

---
+++ src/Components/redux/action.jsx
@@ -0,0 +1,13 @@
+export const addToCart = (service) => {
+  return {
+    type: "ADD_TO_CART",
+    payload: service,
+  };
+};
+
+export const removeFromCart = (service) => {
+  return {
+    type: "REMOVE_FROM_CART",
+    payload: service,
+  };
+};
